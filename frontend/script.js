bb921cc4469ac0c286bc09b641f1fbc8077d5f97
Anthony Colaiacovo <anthonyecolaiacovo@gmail.com>
2023-07-16 20:51:45 -0400

    updating javascript

--- frontend/script.js
+++ frontend/script.js
@@ -7,7 +7,7 @@ $(document).ready(
                 event.preventDefault(); 
                 var form_data = new FormData(); 
                 form_data.append("sdf_file", $("#sdf_file_input")[0].files[0]); 
-                form_data.append("molecule_name", $("#sdf_molecule_name").val())
+                form_data.append("molecule_name", $("#sdf_molecule_name").val());
                 $.ajax(
                     {
                         url: "/sdf-form", 
@@ -19,7 +19,59 @@ $(document).ready(
                 );
             }
         );
-    }
+    },
+
+    function(){
+        $("#add_form").on("submit", 
+            function(event)
+            {
+                event.preventDefault(); 
+                var form_data = new FormData(); 
+                form_data.append("code", $("#ele_code").val()); 
+                form_data.append("name", $("#ele_name").val()); 
+                form_data.append("red",  $("#ele_r").val()); 
+                form_data.append("green",$("#ele_g").val()); 
+                form_data.append("blue", $("#ele_b").val()); 
+                form_data.append("rad",  $("#ele_rad").val()); 
+                
+                $.ajax(
+                    {
+                        url: "/add-form", 
+                        type: "POST",
+                        data: form_data, 
+                        processData: false, 
+                        contentType: false
+                    }
+                );
+            }
+        );
+    }, 
+
+    function(){
+        $("#delete_form").on("submit", 
+            function(event)
+            {
+                event.preventDefault(); 
+                var form_data = new FormData(); 
+                form_data.append("code", $("#rm_code").val()); 
+                form_data.append("name", $("#rm_name").val()); 
+                form_data.append("red",  $("#rm_r").val()); 
+                form_data.append("green",$("#rm_g").val()); 
+                form_data.append("blue", $("#rm_b").val()); 
+                form_data.append("rad",  $("#rm_rad").val()); 
+                
+                $.ajax(
+                    {
+                        url: "/add-form", 
+                        type: "POST",
+                        data: form_data, 
+                        processData: false, 
+                        contentType: false
+                    }
+                );
+            }
+        );
+    } 
 ); 
 
 
